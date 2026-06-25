762d656e7890809dd95dbec3b9db957b10ae9adb
YoussefMohammed <youssif.sayed05@eng-st.cu.edu.eg>
2026-06-26 02:50:50 +0300

    feat: implemented 2x1 MUX

---
+++ src/blocks/ANDOR.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/26/2026 02:19:24 AM
+// Design Name: 
+// Module Name: ANDOR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ANDOR(
+    input A,
+    input B,
+    input C,
+    output F
+    );
+    
+    wire AB;
+    assign AB = A & B;
+    assign F = C & AB;
+    
+endmodule
---
+++ src/blocks/mux_2x1.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/26/2026 02:33:35 AM
+// Design Name: 
+// Module Name: mux_2x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux_2x1(
+    input D0,
+    input D1,
+    input S,
+    output Y
+    );
+    
+    wire not_S;
+    wire out_G2;
+    wire out_G3;
+    
+//First implementation 
+
+    notgate G1 (
+    .A(S),
+    .B(not_S)
+    );
+    
+    andgate G2 (
+    .A(D0),
+    .B(not_S),
+    .C(out_G2)
+    );
+    
+    andgate G3 (
+    .A(D1),
+    .B(S),
+    .C(out_G3)
+    );
+    
+    orgate G4 (
+    .A(out_G2),
+    .B(out_G3),
+    .C(Y)
+    );
+    
+    
+//Second implementation (easier one imo)
+
+    /*
+  assign not_S = ~S;
+  assign out_G2 = D0 & not_S;
+  assign out_G3 = D1 & S;
+  
+  assign Y = out_G2 | out_G3 ;
+    */
+    
+endmodule
