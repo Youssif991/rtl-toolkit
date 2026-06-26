9efa4f5cdd0c3c050242719ff93b432582365efe
YoussefMohammed <youssif.sayed05@eng-st.cu.edu.eg>
2026-06-26 03:11:29 +0300

    test: added a tb for AND and OR

---
+++ tb/gates/tb_andgate.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/26/2026 02:13:34 AM
+// Design Name: 
+// Module Name: tb_andgate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_andgate;
+
+reg     A;
+reg     B;
+wire    C;
+
+andgate dut (
+.A(A),
+.B(B),
+.C(C)
+);
+
+initial begin
+
+#10 A = 0; B =0;
+#10 A = 0; B =1;
+#10 A = 1; B =0;
+#10 A = 1; B =1;
+
+end
+
+endmodule
