e28f7554e5c42ae8fb1cb5caa981dd5e339d7773
YoussefMohammed <youssif.sayed05@eng-st.cu.edu.eg>
2026-06-26 18:59:19 +0300

    feat: implemented full adder

---
+++ src/blocks/Full_Adder.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/26/2026 04:22:48 PM
+// Design Name: 
+// Module Name: Full_Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Full_Adder(
+    input A,
+    input B,
+    input Cin,
+    output Sum,
+    output Cout
+    );
+    
+    wire AB_xor;
+    wire AB_and;
+    wire AB_Cin;
+    
+    assign AB_xor = A ^ B;
+    assign AB_and = A & B;
+    assign AB_Cin = Cin & AB_xor;
+    
+    assign Sum = AB_xor ^ Cin;
+    assign Cout = AB_Cin | AB_and;
+    
+endmodule
