2c5f8b0afb4395f57368bb02e3a277bde8f06dc9
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-07-01 21:15:19 +0300

    feat: enhance mux_bus module with detailed description and parameterization

--- src/blocks/mux_bus.v
+++ src/blocks/mux_bus.v
@@ -6,8 +6,9 @@
 // Design Name: 
 // Module Name: mux_bus
 // Tool Versions: Vivado 2025.2
-// Description: Insert description here
-// 
+// Description: This module is a multiplexer that selects one of the input buses based on the selection signal.
+// The width of the bus, number of input buses, and the number of selection lines can be parameterized.    
+// The output is selected by using the selection signal to index into the concatenated input buses.
 // Dependencies: 
 // 
 // Revision:
@@ -17,13 +18,15 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module mux_bus #(
-parameter bus = 4,
-parameter selection = $clog2 (bus)
-)(
-    input [bus - 1:0] in,
+    parameter bus = 4,
+    parameter inputs = 4,
+    parameter selection = $clog2(inputs) // Calculate the number of selection lines needed based on the number of inputs
+) (
+    input [bus*inputs - 1:0] in,
     input [selection - 1:0] s,
     output [bus - 1:0] out
-    );
-    
-    
+);
+
+  assign out = in[s*bus+:bus]; // Select the appropriate bus based on the selection signal
+
 endmodule
