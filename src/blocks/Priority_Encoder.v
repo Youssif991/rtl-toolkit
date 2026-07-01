461b6ce58b86c8e26e577cb750b72ef4d519fde7
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-07-01 22:56:54 +0300

    style: improve code formatting and comments in Priority Encoder module

--- src/blocks/Priority_Encoder.v
+++ src/blocks/Priority_Encoder.v
@@ -17,21 +17,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module Priority_Encoder #(
-    parameter bus = 8, // Width of each input bus
-    parameter inputs = 4 // Number of input buses
+    parameter bus = 8,  // Width of each input bus
+    parameter inputs = 4  // Number of input buses
 ) (
-    input [bus * inputs - 1 : 0] in, // Concatenated input buses
-    output reg [bus - 1 : 0] out // Output bus
+    input [bus * inputs - 1 : 0] in,  // Concatenated input buses
+    output reg [bus - 1 : 0] out  // Output bus
 );
 
-  integer i; // Declaring the loop variable
+  integer i;  // Declaring the loop variable
 
   always @(*) begin
-    out = 0;
 
-    for (i = 0; i < inputs; i = i + 1) begin
-      if (in[i*bus+:bus] != 0) begin
-        out = in[i*bus+:bus];
+    out = 0;  // Initialize output to zero (default value)
+
+    for (i = 0; i < inputs; i = i + 1) begin  // Loop through each input bus
+      if (in[i*bus+:bus] != 0) begin  // Check if the current input bus is non-zero
+        out = in[i*bus+:bus];  // Assign the non-zero input bus to the output
       end
     end
   end
