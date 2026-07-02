31e1381d8db76199a740a95028c2f983b4aedf46
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-07-02 23:49:26 +0300

    feat: added JK flipflop implementation

---
+++ src/blocks/jk_ff.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Youssef
+// 
+// Create Date: 07/02/2026 23:29:04
+// Design Name: 
+// Module Name: jk_ff
+// Tool Versions: Vivado 2025.2
+// Description: Insert description here
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module jk_ff (
+    input j,
+    input k,
+    input clk,
+    input rstn,
+    output reg q,
+    output reg q_bar
+);
+
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      q <= 1'b0;
+      q_bar <= 1'b1;
+    end else begin
+      case ({
+        j, k
+      })
+        2'b00: begin
+          // Mantain current state
+          q <= q;
+          q_bar <= q_bar;
+        end
+        2'b01: begin
+          // Reset state (Q = 0)
+          q <= 1'b0;
+          q_bar <= 1'b1;
+        end
+        2'b10: begin
+          // Set state (Q = 1)
+          q <= 1'b1;
+          q_bar <= 1'b0;
+        end
+        2'b11: begin
+          // Toggle state
+          q <= ~q;
+          q_bar <= ~q_bar;
+        end
+      endcase
+    end
+  end
+
+endmodule
