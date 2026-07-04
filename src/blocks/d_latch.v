24033370190bc65ae92896e300e94c29ae365285
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-07-04 18:06:32 +0300

    feat: add D-latch module implementation

---
+++ src/blocks/d_latch.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Youssef
+// 
+// Create Date: 07/04/2026 18:04:38
+// Design Name: D-latch
+// Module Name: d_latch
+// Tool Versions: Vivado 2025.2
+// Description: A D-latch that captures the value of `D` when `en` is high and holds it when `en` is low. 
+// The output `Q` reflects the latched value, while `Qn` provides the inverted output.
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module d_latch (
+    input d,
+    input en,
+    input rstn,
+    output reg q
+);
+
+  always @(*) begin
+    if (!rstn) begin
+      q <= 1'b0;
+    end else if (en) begin
+      q <= d;
+    end
+  end
+
+endmodule
