38b7dfc7d3603353034a03b0f8179d11d521eb41
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-07-04 15:22:46 +0300

    docs: update design name and description in ANDOR module

--- src/blocks/ANDOR.v
+++ src/blocks/ANDOR.v
@@ -3,10 +3,11 @@
 // Engineer: Youssef
 // 
 // Create Date: 06/26/2026 02:19:24 AM
-// Design Name: 
+// Design Name: ANDOR - This module implements the logic function F = C | (A & B).
 // Module Name: ANDOR
 // Tool Versions: Vivado 2025.2
-// Description: Insert description here
+// Description: Implements F = C | (A & B). Computes A & B then ORs
+//              the result with C. Combinational logic, single-bit I/O.
 // 
 // Dependencies: 
 // 
@@ -25,6 +26,6 @@ module ANDOR (
 
   wire AB;
   assign AB = A & B;
-  assign F  = C & AB;
+  assign F  = C | AB;
 
 endmodule
