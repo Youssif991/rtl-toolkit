6b26d523e31c37e31a9bd353c19701cdd47a1074
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-07-05 19:23:32 +0300

    feat: add Johnson Counter module implementation

---
+++ src/blocks/johnson_ctr.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Youssef
+// 
+// Create Date: 07/05/2026 18:54:19
+// Design Name: Johnson Counter
+// Module Name: johnson_ctr
+// Tool Versions: Vivado 2025.2
+// Description: Insert description here
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module johnson_ctr #(
+    parameter N = 4
+) (
+    input clk,
+    input rstn,
+    output reg [N - 1 : 0] out
+);
+
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) out <= 0;
+    else begin
+
+      out[N-1] <= ~out[0]; // in order to have the shifting pattern of the johnson counter
+
+      for (integer i = 0; i < N - 1; i = i + 1) begin : counting_loop
+        out <= out[i+1];
+      end
+    end
+  end
+
+endmodule
