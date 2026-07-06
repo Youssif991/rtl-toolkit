946473afd9ba727fe62993e51eb603c1a4ad803c
Youssif991 <youssif.sayed05@eng-st.cu.edu.eg>
2026-07-06 17:54:16 +0300

    fix: correct output type and improve formatting in seven segment display module

--- src/blocks/seven_seg.v
+++ src/blocks/seven_seg.v
@@ -17,28 +17,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module seven_seg #(
-    parameter active_low = 0 // parameter to toggle between active high or active low
+    parameter active_low = 0  // parameter to toggle between active high or active low
 
 ) (
-    input [3 : 0] in,
-    output reg [6 : 0] out
+    input  [3 : 0] in,
+    output [6 : 0] out
 );
-  reg out_active_high;
+  reg [6 : 0] out_active_high;
   always @(*) begin : seven_segment_cases
     case (in)
 
-      4'h0: out_active_high = 7'b1111110; // display 0
-      4'h1: out_active_high = 7'b0110000; // display 1
-      4'h2: out_active_high = 7'b1101101; // display 2
-      4'h3: out_active_high = 7'b1111001; // display 3
-      4'h4: out_active_high = 7'b0110011; // display 4
-      4'h5: out_active_high = 7'b1011011; // display 5
-      4'h6: out_active_high = 7'b1011111; // display 6
-      4'h7: out_active_high = 7'b1110000; // display 7
-      4'h8: out_active_high = 7'b1111111; // display 8
-      4'h9: out_active_high = 7'b1111010; // display 9
-      default: out_active_high = 7'b0000000; // the default display as long as there is no BCD input
- 
+      4'h0: out_active_high = 7'b1111110;  // display 0
+      4'h1: out_active_high = 7'b0110000;  // display 1
+      4'h2: out_active_high = 7'b1101101;  // display 2
+      4'h3: out_active_high = 7'b1111001;  // display 3
+      4'h4: out_active_high = 7'b0110011;  // display 4
+      4'h5: out_active_high = 7'b1011011;  // display 5
+      4'h6: out_active_high = 7'b1011111;  // display 6
+      4'h7: out_active_high = 7'b1110000;  // display 7
+      4'h8: out_active_high = 7'b1111111;  // display 8
+      4'h9: out_active_high = 7'b1111010;  // display 9
+      default:
+      out_active_high = 7'b0000000;  // the default display as long as there is no BCD input
+
     endcase
   end
 
